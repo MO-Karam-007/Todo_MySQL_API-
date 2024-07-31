a60d1cb2f8d14e7cb151c5fe70779ff86e8d522d
Mohamed <karam.hamam95@gmail.com>
2024-07-31 20:34:54 +0300

    validation

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,25 @@ var express = require('express');
 var router = express.Router();
 const { getTodos, getTodo, createTodo, updateTodo, deleteTodo } = require('../services/factory')
 
+function validation() {
+  return (req, res, nxt) => {
+    if (!req.body.title || typeof req.body.title !== "string") {
+      return res.status(400).json({ error: "Invalid title" });
+    }
+    if (typeof req.body.completed !== "boolean") {
+      return res.status(400).json({ error: "Invalid completed status" });
+    }
+    nxt();
+  };
+}
 // Routes
 router.route('/')
   .get(getTodos)
-  .post(createTodo);
+  .post(validation(), createTodo);
 
 router.route('/:id')
   .get(getTodo)
-  .put(updateTodo)
+  .put(validation(), updateTodo)
   .delete(deleteTodo);
 
 module.exports = router;
